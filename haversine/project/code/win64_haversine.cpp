c4e03ca71e1b32d1924d1886448b6ffed9f72564
Leta <31357777+Lita6@users.noreply.github.com>
2023-03-14 14:47:10 -0500

    Converts a float to text.
    Made sure that RangedRandomR32 doesn't go outside the indicated range.
    
    Extracted the generation of 40 mil numbers to its own function. And hardcoded the page-aligned size.
    
    Instead of generating the numbers every single time, it now writes it to file as needed.
    
    Converts the number up to six decimal places into text.

--- haversine/project/code/win64_haversine.cpp
+++ haversine/project/code/win64_haversine.cpp
@@ -48,9 +48,38 @@ RangedRandomR32
 		randomR32 /= 10.0;
 		randomR32 += ((r32)rand / (r32)16.0);
 		
+		if(randomR32 >= 1.0)
+		{
+			randomR32 -= 1.0;
+		}
+		
 	}
 	
 	r32 result = ((randomR32 * (max - min)) + min);
+	Assert(result <= max);
+	return(result);
+}
+
+Buffer
+Generate40Mil
+()
+{
+	
+	Buffer result = win64_make_buffer(160002048, PAGE_READWRITE);
+	Assert(result.memory != 0);
+	
+	u32 count = 0;
+	while(count < 40000000)
+	{
+		
+		buffer_append_r32(&result, RangedRandomR32(-180.0, 180.0));
+		count++;
+		
+		buffer_append_r32(&result, RangedRandomR32(-90.0, 90.0));
+		count++;
+		
+	}
+	
 	return(result);
 }
 
@@ -66,26 +95,68 @@ WinMainCRTStartup
 	
 	SEED = (u64)0x0123456789ABCDEF;
 	
-	s32 size_r32 = sizeof(r32);
-	u32 buffer_size = (40000000 * (u32)size_r32);
-	Buffer coordinates = win64_make_buffer(AlignSize(buffer_size, PAGE), PAGE_READWRITE);
+#if 0
+	Buffer coordinates = Generate40Mil();
 	
+	b32 WriteSucceeded = Win64WriteEntireFile("d:\\programming\\github\\haversine-thing\\haversine\\build\\fortymil.array", coordinates.size, coordinates.memory);
+	Assert(WriteSucceeded == TRUE);
+#endif
+	
+	read_file_result pairs = Win64ReadEntireFile("d:\\programming\\github\\haversine-thing\\haversine\\build\\fortymil.array");
+	Assert(pairs.Contents != 0);
+	
+	r32 current = 0.0;
+	r32 *num = (r32 *)pairs.Contents;
 	u32 count = 0;
 	while(count < 40000000)
 	{
 		
-		r32 minLong = -180.0;
-		r32 maxLong = 180.0;
-		r32 randLong = RangedRandomR32(minLong, maxLong);
-		buffer_append_r32(&coordinates, randLong);
-		count++;
+		u8 chars[11] = {};
+		u8 c = 0;
+		current = *num;
 		
-		r32 minLat = -90.0;
-		r32 maxLat = 90.0;
-		r32 randLat = RangedRandomR32(minLat, maxLat);
-		buffer_append_r32(&coordinates, randLat);
-		count++;
+		if(current < 0.0)
+		{
+			chars[c++] = '-';
+			current *= -1.0;
+		}
 		
+		s32 nonDecimal = (s32)current;
+		if(nonDecimal == 0)
+		{
+			chars[c++] = '0';
+		}
+		else
+		{
+			current -= (r32)nonDecimal;
+			
+			s32 mod = 100;
+			while(mod != 0)
+			{
+				
+				s32 digit = nonDecimal / mod;
+				if(digit != 0)
+				{
+					chars[c++] = (u8)(digit + '0');
+					nonDecimal -= (digit * mod);
+				}
+				
+				mod /= 10;
+			}
+		}
+		
+		chars[c++] = '.';
+		
+		for(u32 i = 0; i < 6; i++)
+		{
+			current *= 10;
+			s32 digit = (s32)current;
+			current -= (r32)digit;
+			chars[c++] = (u8)(digit + '0');
+		}
+		
+		num++;
+		count++;
 	}
 	
 	return(0);
