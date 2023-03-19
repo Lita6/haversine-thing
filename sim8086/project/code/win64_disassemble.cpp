6c16b1a3c609f443f24aaed1b75c55e04133541c
Leta <31357777+Lita6@users.noreply.github.com>
2023-03-19 18:23:03 -0500

    Now disassembles many lines

--- sim8086/project/code/win64_disassemble.cpp
+++ sim8086/project/code/win64_disassemble.cpp
@@ -63,10 +63,22 @@ WinMainCRTStartup
 	List regs = {};
 	regs.start = (List_Entry *)buffer_reserved.end;
 	regs.buffer = &buffer_reserved;
-	create_list_entry(&buffer_strings, &regs, "ax", 0b000);
-	create_list_entry(&buffer_strings, &regs, "cx", 0b001);
-	create_list_entry(&buffer_strings, &regs, "dx", 0b010);
-	create_list_entry(&buffer_strings, &regs, "bx", 0b011);
+	create_list_entry(&buffer_strings, &regs, "ax", 0b1000);
+	create_list_entry(&buffer_strings, &regs, "cx", 0b1001);
+	create_list_entry(&buffer_strings, &regs, "dx", 0b1010);
+	create_list_entry(&buffer_strings, &regs, "bx", 0b1011);
+	create_list_entry(&buffer_strings, &regs, "sp", 0b1100);
+	create_list_entry(&buffer_strings, &regs, "bp", 0b1101);
+	create_list_entry(&buffer_strings, &regs, "si", 0b1110);
+	create_list_entry(&buffer_strings, &regs, "di", 0b1111);
+	create_list_entry(&buffer_strings, &regs, "al", 0b000);
+	create_list_entry(&buffer_strings, &regs, "cl", 0b001);
+	create_list_entry(&buffer_strings, &regs, "dl", 0b010);
+	create_list_entry(&buffer_strings, &regs, "bl", 0b011);
+	create_list_entry(&buffer_strings, &regs, "ah", 0b100);
+	create_list_entry(&buffer_strings, &regs, "ch", 0b101);
+	create_list_entry(&buffer_strings, &regs, "dh", 0b110);
+	create_list_entry(&buffer_strings, &regs, "bh", 0b111);
 	
 	u8 *commandLine = (u8 *)GetCommandLineA();
 	commandLine += 0x4c;
@@ -79,18 +91,27 @@ WinMainCRTStartup
 	(void)EndOfFile;
 	
 	u8 *ReadByte = (u8 *)file.Contents;
+	while(ReadByte < EndOfFile)
+	{
+		
+		create_string(&program, "mov ");
+		u8 w = (u8)((*ReadByte & 0b01) << 3);
+		ReadByte++;
+		
+		u8 dest = (u8)((*ReadByte & 0b111) | w);
+		List_Entry *dest_reg = find_reg(&regs, dest);
+		append_string(&program, dest_reg->name);
+		create_string(&program, ", ");
+		
+		u8 src = (u8)(((*ReadByte & (0b111 << 3)) >> 3) | w);
+		List_Entry *src_reg = find_reg(&regs, src);
+		append_string(&program, src_reg->name);
+		
+		buffer_append_u8(&program, '\n');
+		ReadByte++;
+	}
 	
-	create_string(&program, "mov ");
-	ReadByte++;
-	
-	u8 dest = (u8)(*ReadByte & 0b111);
-	List_Entry *dest_reg = find_reg(&regs, dest);
-	append_string(&program, dest_reg->name);
-	create_string(&program, ", ");
-	
-	u8 src = (u8)((*ReadByte & (0b111 << 3)) >> 3);
-	List_Entry *src_reg = find_reg(&regs, src);
-	append_string(&program, src_reg->name);
+	*(program.end - 1) = 0;
 	
 	String dsmFileName = create_string(&buffer_strings, "dsm_");
 	append_string(&buffer_strings, fileName);
